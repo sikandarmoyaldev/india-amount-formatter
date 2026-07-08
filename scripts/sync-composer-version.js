58c84948dfaf35c7737cdc7510db95f98582fff3
Sikandar <sikandar.moyal.dev@gmail.com>
2026-07-09 01:00:15 +0530

    chore: finalize monorepo setup for npm and Packagist publishing

--- scripts/sync-composer-version.js
+++ scripts/sync-composer-version.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 // 1. Read the new version that Release It just put in the ROOT package.json
-const rootPackagePath = path.resolve(__dirname, "../package.json");
-const rootPackage = JSON.parse(fs.readFileSync(rootPackagePath, "utf-8"));
+const rootPackagePath = path.resolve(__dirname, '../package.json');
+const rootPackage = JSON.parse(fs.readFileSync(rootPackagePath, 'utf-8'));
 const newVersion = rootPackage.version;
 
 // 2. Update the TS package.json
-const tsPackagePath = path.resolve(__dirname, "../packages/ts/package.json");
-const tsPackage = JSON.parse(fs.readFileSync(tsPackagePath, "utf-8"));
+const tsPackagePath = path.resolve(__dirname, '../packages/ts/package.json');
+const tsPackage = JSON.parse(fs.readFileSync(tsPackagePath, 'utf-8'));
 tsPackage.version = newVersion;
-fs.writeFileSync(tsPackagePath, JSON.stringify(tsPackage, null, 2) + "\n");
+fs.writeFileSync(tsPackagePath, JSON.stringify(tsPackage, null, 2) + '\n');
 
-// 3. Update the PHP composer.json
-const composerPath = path.resolve(__dirname, "../packages/php/composer.json");
-const composer = JSON.parse(fs.readFileSync(composerPath, "utf-8"));
+// 3. Update the PHP composer.json (NOW IN THE ROOT!)
+const composerPath = path.resolve(__dirname, '../composer.json');
+const composer = JSON.parse(fs.readFileSync(composerPath, 'utf-8'));
 composer.version = newVersion;
-fs.writeFileSync(composerPath, JSON.stringify(composer, null, 4) + "\n");
+fs.writeFileSync(composerPath, JSON.stringify(composer, null, 4) + '\n');
 
-console.log(`Synced TS and PHP packages to v${newVersion}`);
+console.log(`Synced TS package and root composer.json to v${newVersion}`);
